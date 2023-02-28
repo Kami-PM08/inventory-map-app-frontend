b1a5120cfca0b9f39b3c21e8d759be04a09b58d7
CristianPedraza <37862210+Kami-PM08@users.noreply.github.com>
2023-02-28 17:20:13 -0500

    project structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import useLocation from "./utils/useLocation";
+// import useSWR from "swr";
 
-function App() {
+
+
+// const DEFAULT_LOCATION = {
+//   latitude: 4.712977399850152,
+//   longitude: -74.04462831952155,
+// };
+
+// const getLocation = async () => {
+//   return new Promise((resolve, reject) => {
+//     navigator.geolocation.getCurrentPosition(
+//       ({ coords }) => {
+//         const currentLocation = {
+//           longitude: coords.longitude,
+//           latitude: coords.latitude,
+//         };
+//         console.log(currentLocation);
+//         resolve(currentLocation);
+//       },
+//       (error) => {
+//         alert("Error al obtener ubicación. Seutilizará una por defecto");
+//         console.error("Error al obtener ubicación", error);
+//         reject();
+//       }
+//     );
+//   });
+// };
+const App = () => {
+
+  
+  // const useLocation = () => {
+  //   // if (!navigator.geolocation) {
+  //   //   return {
+  //   //     isLoading: false,
+  //   //     currentLocation: DEFAULT_LOCATION,
+  //   //   };
+  //   // }
+  //   const { data, error, isLoading } = useSWR("currentLocation", getLocation);
+  //   return {
+  //     isLoading,
+  //     currentLocation: error ? DEFAULT_LOCATION : data,
+  //   };
+  // };
+
+  const { isLoading, currentLocation } = useLocation();
+  console.log("dataLoc", currentLocation);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hola mundo</h1>
+      {isLoading ? <p>Cargando...</p> : <p>Lng{currentLocation.longitude}</p>}
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/utils/useLocation.js
@@ -0,0 +1,36 @@
+import useSWR from "swr";
+
+const DEFAULT_LOCATION = {
+  latitude: 4.712977399850152,
+  longitude: -74.04462831952155,
+};
+
+const getLocation = async () => {
+  return new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(
+      ({ coords }) => {
+        const currentLocation = {
+          longitude: coords.longitude,
+          latitude: coords.latitude,
+        };
+        console.log(currentLocation);
+        resolve(currentLocation);
+      },
+      (error) => {
+        alert("Error al obtener ubicación. Seutilizará una por defecto");
+        console.error("Error al obtener ubicación", error);
+        reject();
+      }
+    );
+  });
+};
+
+const useLocation = () => {
+  const { data, error, isLoading } = useSWR("currentLocation", getLocation);
+  return {
+    isLoading,
+    currentLocation: error ? DEFAULT_LOCATION : data,
+  };
+};
+
+export default useLocation;
